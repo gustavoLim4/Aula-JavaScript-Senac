d10fda4657103599d9c31f8b29f237e257da5d43
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2024-04-20 12:01:18 -0300

    arrumei o nome da variavel

--- aula-03/js/PegarAimgem.js
+++ aula-03/js/PegarAimgem.js
@@ -1,7 +1,7 @@
-var mini = document.getElementsByClassName("mini")[0]
+var fotoNav = document.getElementsByClassName("fotoNav")[0]
 // var full = document.getElementsByClassName("full")[0]
 
 
 for (var i = 1; i <= 5; i++) {
-    mini.innerHTML += "<img src =img/foto"+i+".jpg>"
+    fotoNav.innerHTML += "<img src =img/foto"+i+".jpg>"
 }
