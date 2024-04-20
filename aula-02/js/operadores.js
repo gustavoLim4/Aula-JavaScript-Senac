ddd3655b35f8d43be1b1df728bf1b7335ef4794a
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2024-04-20 08:02:49 -0300

    algumas alterações

--- aula-02/js/operadores.js
+++ aula-02/js/operadores.js
@@ -9,4 +9,6 @@ subtrair = numero1 - numero2
 multplicar = numero1 * numero2
 dividir = numero1 / numero2
 resto = numero1 % numero2
-document.getElementsByTagName("div")[0].innerHTML = soma + "<br>" +multplicar+ "<br>" +dividir+ "<br>" +resto ;
+document.getElementsByTagName("div")[0].innerHTML = (soma + "<br>" +multplicar+ "<br>" +dividir+ "<br>" +resto) 
+
+//                                                              INDEX 4
