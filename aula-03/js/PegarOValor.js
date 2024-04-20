e07598ecfdcd5b6902669b1d05e01daee47f9b05
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2024-04-20 11:05:14 -0300

    pegando informações do input usando getElementsByTagNmae e innerHTML e tambem usando a função onClink e OnBlur

---
+++ aula-03/js/PegarOValor.js
@@ -0,0 +1,36 @@
+// function verificar() {
+
+//     var nota = document.getElementsByTagName("input")[0].value
+
+//     var resultado = "Aprovado"
+
+//     if (nota >= 7) {
+//         resultado
+//     }
+//     else if (nota <= 4){
+//         resultado = "Reprovado"
+//     }
+//     else {
+//         resultado = "Recuperção"
+//     }
+//     document.getElementsByTagName("label")[1].innerHTML = resultado
+// }
+
+function resultado () {
+    
+
+    var nota = document.getElementsByTagName("input")[0].value
+
+    var resultado = "Aprovado"
+
+    if (nota >= 7) {
+        resultado
+    }
+    else if (nota <= 4){
+        resultado = "Reprovado"
+    }
+    else {
+        resultado = "Recuperção"
+    }
+    document.getElementsByTagName("label")[1].innerHTML = resultado
+}
