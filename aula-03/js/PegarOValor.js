4d6a05b49fb4cce36132120187ed64109bedf9eb
gustavoLim4 <gustavolll15567@gmail.com>
2024-04-22 15:38:27 -0300

    aula-04 adicionada

--- aula-03/js/PegarOValor.js
+++ aula-03/js/PegarOValor.js
@@ -1,20 +1,4 @@
-// function verificar() {
 
-//     var nota = document.getElementsByTagName("input")[0].value
-
-//     var resultado = "Aprovado"
-
-//     if (nota >= 7) {
-//         resultado
-//     }
-//     else if (nota <= 4){
-//         resultado = "Reprovado"
-//     }
-//     else {
-//         resultado = "Recuperção"
-//     }
-//     document.getElementsByTagName("label")[1].innerHTML = resultado
-// }
 
 function resultado () {
     
