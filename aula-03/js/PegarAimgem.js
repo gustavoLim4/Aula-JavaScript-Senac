c58041c83f692ff34f67836ad4e84ab412bc4e10
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2024-04-27 11:03:15 -0300

    o onclick foi adicionado para que venha estar aparecendo em baixo da FotoNav a imagem que foi clicada

--- aula-03/js/PegarAimgem.js
+++ aula-03/js/PegarAimgem.js
@@ -1,20 +1,12 @@
-var fotoNav = document.getElementsByClassName("fotoNav")[0]
-var full = document.getElementsByClassName("full")[0]
+//Atribuindo a class "fotoNav" para uma variavel 
+var fotoNav = document.getElementById("fotoNav") 
+var full = document.getElementById("full")
 
-
+//laço de repetição com a variavel "i" que termina quando chegar no 5 que é de acordo de quantas images tem
 for (var i = 1; i <= 5; i++) {
     fotoNav.innerHTML += "<img src =img/foto"+i+".jpg onclick = abrir("+i+")>"
 }
-
-
-// document.getElementsByTagName("img")[0].onclick = function() {
-//   full.innerHTML = "<img src = img/foto1.jpg>"
-// }
-// document.getElementsByTagName("img")[1].onclick = function() {
-//   full.innerHTML = "<img src = img/foto2.jpg>"
-// }
-
-
+//função abrir que vem do onclick "i" e variavel "p" recebe o numero que o usuario clicar 
 function abrir (p) { 
-    full.innerHTML = "<img src = img/foto"+(p)+".jpg>"
+    full.innerHTML = "<img src = img/foto"+p+".jpg>"
 }
