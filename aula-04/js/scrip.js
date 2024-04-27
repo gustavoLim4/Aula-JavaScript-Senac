8d9c1f5e76e29f4afe840269ceedc447839d49e9
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2024-04-27 12:09:47 -0300

    fizemos um carrossel em javascript na aula04

---
+++ aula-04/js/scrip.js
@@ -0,0 +1,26 @@
+var trocasIMG = 1
+// na variavel global para acionar a função do botão reduz a variavel e faz a troca no innerHTMl pela variavel posicao
+function esquerda () {
+
+    if (trocasIMG <= 1 ) {
+        trocasIMG = 1
+    }
+    else{
+        trocasIMG--
+    }
+    
+    document.getElementById("foto").innerHTML = "<img src = img/foto"+trocasIMG+".jpg>"
+}
+
+
+function direita () {
+    if (trocasIMG <= 5 ) {
+        trocasIMG = 5
+    }
+    else{
+        trocasIMG++
+    }
+
+    
+    document.getElementById("foto").innerHTML = "<img src = img/foto"+trocasIMG+".jpg>"
+}
