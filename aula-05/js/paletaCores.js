2dd617f29348a6bf990a30f4ffa241b111c1c295
GUSTAVO LIMA DA SILVA SANTOS <gustavo.lssantos3@senacsp.edu.br>
2024-05-11 09:44:12 -0300

    aula - 06

--- aula-05/js/paletaCores.js
+++ aula-05/js/paletaCores.js
@@ -5,15 +5,21 @@ const painel = document.getElementById("painel")
 let rs = "<table>";
 for (let linha = 1; linha <= 10; linha++) {
     rs += "<tr>"
-    for (let colum = 1; colum <= 30; colum++) {
+    for (let colum = 1; colum <=    60; colum++) {
         let r = parseInt(Math.random() * 255)
         let g = parseInt(Math.random() * 255)
         let b = parseInt(Math.random() * 255)
-        rs += "<td style=background-color:rgb("+r+","+g+","+b+")>cor</td>"
+        rs += "<td onClick = mudarCor("+r+","+g+","+b+") style=background-color:rgb("+r+","+g+","+b+")>     </td>"
     }
     rs += "</tr>"
 }
 rs += "</table>"
 
 paleta.innerHTML = rs
+
+
+function mudarCor(red , green , blue) {
+    // console.log(red , green , blue)
+    painel.style.backgroundColor=`rgb(${red}, ${green},  ${blue})`
+}
 
---
+++ aula-06/js/script.js
@@ -0,0 +1,26 @@
+function array1() {
+    let num = [1 ,2 ,3 , 4 ,5 , "gustavo"]
+    //exibir a quantidade de elementos de uma array
+    console.log(num.length)
+    //Exibir o primeiro elemento d array 
+    console.log(num[5])
+}
+function array2() {
+    let num = [10 ,20 ,30 , 45 ,55]
+    for (let i = 0 ; i <= 4 ; i++) {
+        if (num[i] % 2 == 0){
+            console.log(num[i])
+        }
+    //         return(
+    //             <p>Par</p>
+    //         )
+    //     }
+    // else{
+    //     return (
+    //         <p>Impar</p>
+    //     )
+    // }
+   }
+}
+
+
